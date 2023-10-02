c8eb7ce68c7460e4c66e3c41da61ba4b2483f1a9
Vitor Lucas <vitorcrispim@yahoo.com.br>
2023-10-02 18:52:25 -0300

    Iniciando o projeto

---
+++ src/main/java/com/uber/challenge/vitor/uberChallenger/UberChallengerApplication.java
@@ -0,0 +1,13 @@
+package com.uber.challenge.vitor.uberChallenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberChallengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberChallengerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uber/challenge/vitor/uberChallenger/UberChallengerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uber.challenge.vitor.uberChallenger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberChallengerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
